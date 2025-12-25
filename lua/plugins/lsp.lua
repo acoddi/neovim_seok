a8f597f91312dab38d5da7e2640b48c2d50eb828
acoddi <2501ksh@gmail.com>
2025-12-25 20:16:42 +0900

    12/25 finish

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,20 +7,24 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		dependencies = {
 			"williamboman/mason.nvim",
-			"neovim/nvim-lspconfig", -- 데이터베이스 역할로만 사용
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp", -- [추가] 자동 완성을 위해 이 라이브러리가 필요합니다
 		},
 		config = function()
 			local mason_lspconfig = require("mason-lspconfig")
 
-			-- 1. Mason 설치 설정
+			-- 1. [추가] 자동 완성을 위한 통신 통로(capabilities) 설정
+			-- 이 설정이 들어가야 LSP가 "나 이런 자동 완성 줄 수 있어!"라고 cmp에게 알려줍니다.
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			-- 2. Mason 설치 설정
 			mason_lspconfig.setup({
 				ensure_installed = { "clangd", "lua_ls" },
 			})
 
-			-- 2. [미래지향적] lspconfig 프레임워크를 우회하는 네이티브 설정
-			-- lua_ls 설정
+			-- 3. [미래지향적] 네이티브 설정에 capabilities 추가
 			vim.lsp.config("lua_ls", {
-				-- lspconfig의 기본 설정을 가져오되 setup을 호출하지 않음
+				capabilities = capabilities, -- [추가]
 				install = {
 					cmd = { "lua-language-server" },
 				},
@@ -32,14 +36,14 @@ return {
 				},
 			})
 
-			-- clangd 설정
 			vim.lsp.config("clangd", {
+				capabilities = capabilities, -- [추가]
 				install = {
 					cmd = { "clangd" },
 				},
 			})
 
-			-- 3. 서버 활성화 (이 명령어가 예전의 .setup()을 대체합니다)
+			-- 4. 서버 활성화
 			vim.lsp.enable({ "lua_ls", "clangd" })
 		end,
 	},
